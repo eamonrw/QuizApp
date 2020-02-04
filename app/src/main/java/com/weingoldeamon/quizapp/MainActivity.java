7a5d7117321359dc90085d5e7c39d9cf4b1fe4b5
eamonrw <eamonweingold@gmail.com>
2020-02-04 09:04:12 -0500

    Finished Tutorial, changed accent colors

--- app/src/main/java/com/weingoldeamon/quizapp/MainActivity.java
+++ app/src/main/java/com/weingoldeamon/quizapp/MainActivity.java
@@ -12,32 +12,32 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     Button submitButton;
-    TextView textBlock;
-    EditText textBox;
+    TextView displayText;
+    EditText responseText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         submitButton = findViewById(R.id.clickButton);
-        textBlock = findViewById(R.id.textBox);
-        textBox = findViewById(R.id.responseEditText);
+        displayText = findViewById(R.id.textBox);
+        responseText = findViewById(R.id.responseEditText);
 
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
                 System.out.println("Hello mom");
-                Log.i("testButton","Hi dad! "+textBox.getText().toString());
-                textBlock.setText(textBlock.getText().toString()+" "+textBox.getText().toString());
+                Log.i("testButton","Hi dad! "+ responseText.getText().toString());
+                displayText.setText(displayText.getText()+" "+ responseText.getText());
             }
         });
-        textBox.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+        responseText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
-                if(!hasFocus && textBox.getText().toString().equals("TJ")){
-                    textBlock.setText("TJ Rocks!");
-                    textBox.setText("");
-                    textBox.setHint("That's a good name.");
+                if(!hasFocus && responseText.getText().toString().equals("TJ")){
+                    displayText.setText("TJ Rocks!");
+                    responseText.setText("");
+                    responseText.setHint("That's a good name.");
                 }
             }
         });
